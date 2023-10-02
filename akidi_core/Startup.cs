92db74c36851fa5ee129f6a2e21baac0bdf1a36d
Abdoul Dosso <abdoul.dosso@gtbank.com>
2023-10-02 13:57:10 +0200

    email services

--- akidi_core/Startup.cs
+++ akidi_core/Startup.cs
@@ -1,3 +1,5 @@
+using akidi_core.Models;
+using akidi_core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,9 @@ namespace akidi_core
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+            //services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
+            //services.AddTransient<IEmailSender, EmailSender>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
